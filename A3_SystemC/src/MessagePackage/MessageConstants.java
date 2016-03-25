5cf800eb0a2796b6c643d917c80906433535acd7
Shone <shonejin@gmail.com>
2016-03-25 12:39:23 -0400

    Defined constants in MessageConstants.java

--- A3_SystemC/src/MessagePackage/MessageConstants.java
+++ A3_SystemC/src/MessagePackage/MessageConstants.java
@@ -3,7 +3,7 @@ package MessagePackage;
  * The MessageConstants class is used for storing all constant values related to message
  * like message IDs, action code, etc
  * Those variables should be used system wide instead of magic numbers
- * By doing so, we promote maintainability, readability and changability
+ * By doing so, we promote maintainability, readability and changeability
  * */
 public class MessageConstants {
 	static class ID {
